3a732c76d308d1141a7d89787b2c518acd2d785d
Deja <dejadale@gmail.com>
2024-04-18 17:09:40 -0400

    added files

---
+++ destructuring.js
@@ -0,0 +1,16 @@
+8 and 1846
+yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659
+'Your name is Alejandro and you like purple', 'Your name is Melissa and you like green','Your name is undefined and you like green'
+Maya, Marisa, Chi
+'Rain drops on roses', 'whiskers on kittens', everything else is array ['bright copper kettles', 'warm woolen mittens', 'brown paper packages tied up with string']
+[10,30,20]
+const obj = {
+    numbers: {
+        a: 1,
+        b: 2
+    }
+};
+const {a,b} = obj.numbers
+[arr[0], arr[1]] = [arr[1], arr[0]] 
+
+const raceResults = ([first, second, third,...rest]) => ({first, second, third,...rest})
